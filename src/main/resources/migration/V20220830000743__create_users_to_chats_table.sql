f768d95d063ac00eddfb6d48ac961facd4eaf58d
Nicknameee <igrunka1991@gmail.com>
2022-09-03 00:11:40 +0300

    Unique constraint fix

--- src/main/resources/migration/V20220830000743__create_users_to_chats_table.sql
+++ src/main/resources/migration/V20220830000743__create_users_to_chats_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS users_to_chats(
     id BIGSERIAL PRIMARY KEY NOT NULL,
     user_id INT8 REFERENCES users(id),
-    chat_id INT8 REFERENCES chats(id)
+    chat_id INT8 REFERENCES chats(id),
+    UNIQUE(user_id, chat_id)
 )
