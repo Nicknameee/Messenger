1f07b0133a0057ed476d4ec03d3b0ae5102b14b6
Nicknameee <igrunka1991@gmail.com>
2022-11-23 00:08:42 +0200

    Runner for test is declared

--- src/test/java/spring/application/tree/UtilityTest.java
+++ src/test/java/spring/application/tree/UtilityTest.java
@@ -1,11 +1,14 @@
 package spring.application.tree;
 
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @Import(ApplicationTestConfiguration.class)
 @ContextConfiguration(initializers = ApplicationTestContextInitializer.class)
